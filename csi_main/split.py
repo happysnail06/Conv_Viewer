45960d8b5165ba12026b283ccfcfb8d47ef7fc48
김성환 <happy@gimseonghwan-ui-MacBookPro.local>
2025-02-10 16:12:54 +0900

    Updates

--- csi_main/split.py
+++ csi_main/split.py
@@ -2,14 +2,17 @@ import json
 import os
 
 # File name
-# type = 'Passive'
+type = 'Passive'
 # type = 'Active'
-type = 'Less_Active'
-input_filename = f"public/csi_main/raw/CSI_clothing_{type}_150_2025-02-09.json"
+# type = 'Less_Active'
+
+# model = 'chatcrs'
+model = 'CSI'
+input_filename = f"public/csi_main/raw/{model}_clothing_{type}_150_2025-02-09.json"
 
 # Extract the directory name correctly (remove full path and 'CSI_')
 filename_only = os.path.basename(input_filename)  # Get 'CSI_clothing_Active_150_2025-02-09.json'
-dir_name = filename_only.replace("CSI_", "").replace(".json", "")
+dir_name = filename_only.replace(".json", "")
 
 # Define the correct output directory
 output_dir = os.path.join("public/csi_main/split", dir_name)
@@ -23,7 +26,7 @@ with open(input_filename, "r", encoding="utf-8") as file:
 
 # Store each entry as a separate JSON file
 for idx, entry in enumerate(data):
-    output_filename = os.path.join(output_dir, f"{type}_{idx}.json")
+    output_filename = os.path.join(output_dir, f"{model}_{type}_{idx}.json")
     with open(output_filename, "w", encoding="utf-8") as out_file:
         json.dump(entry, out_file, indent=4, ensure_ascii=False)
 
